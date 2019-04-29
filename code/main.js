b5c7555e6192a0875b0c4b879c0130c72788e467
simonjosling <46190255+simonjosling@users.noreply.github.com>
2019-04-28 23:31:21 -0500

    Updated lazy script

--- code/main.js
+++ code/main.js
@@ -16,6 +16,7 @@ function stickyNav() {
   }
 }
 
+//lazy load script
 
 document.addEventListener("DOMContentLoaded", function() {
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
@@ -26,17 +27,60 @@ document.addEventListener("DOMContentLoaded", function() {
         if (entry.isIntersecting) {
           let lazyImage = entry.target;
           lazyImage.src = lazyImage.dataset.src;
-          lazyImage.srcset = lazyImage.dataset.srcset;
+      //  lazyImage.srcset = lazyImage.dataset.srcset;
           lazyImage.classList.remove("lazy");
           lazyImageObserver.unobserve(lazyImage);
         }
-      });
+      }
+ //move fold for lazy load
+ //		,{rootMargin: "0px 0px 256px 0px"}
+ //Uncode this when proved working without
+      );
     });
 
     lazyImages.forEach(function(lazyImage) {
       lazyImageObserver.observe(lazyImage);
     });
   } else {
-    // Possibly fall back to a more compatible method here
+    // Fall back to event listener code for browsers without Intersection Observer support
+    document.addEventListener("DOMContentLoaded", function() {
+    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+    let active = false;
+    
+    const lazyLoad = function() {
+    if (active === false) {
+    active = true;
+    
+    setTimeout(function() {
+    lazyImages.forEach(function(lazyImage) {
+    if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+    lazyImage.src = lazyImage.dataset.src;
+    lazyImage.srcset = lazyImage.dataset.srcset;
+    lazyImage.classList.remove("lazy");
+    
+    lazyImages = lazyImages.filter(function(image) {
+    return image !== lazyImage;
+    });
+    
+    if (lazyImages.length === 0) {
+    document.removeEventListener("scroll", lazyLoad);
+    window.removeEventListener("resize", lazyLoad);
+    window.removeEventListener("orientationchange", lazyLoad);
+    }
+    }
+    });
+    
+    active = false;
+    }, 200);
+    }
+    };
+    
+    document.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+    window.addEventListener("orientationchange", lazyLoad);
+    });
+    
+   
+    //end fallback method
   }
 });
