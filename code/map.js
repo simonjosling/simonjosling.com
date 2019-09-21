dba7f08cf767e8db5dda86fe8bbf9413b51edc24
simonjosling <46190255+simonjosling@users.noreply.github.com>
2019-09-22 01:56:29 +0800

    Add files via upload

--- code/map.js
+++ code/map.js
@@ -55,7 +55,7 @@ var locationsAmerica = [
 	['Cairns',-16.9186,145.7781,39,'#cairns'],
 	['Mission Beach',-17.8694,146.1069,40,'#last'],
 	['Paluma National Park',-19.0919,146.2706,41,'#paluma'],
-	['Agnes Water and 1770',-24.2125,151.9033,43,'#agnes water']
+	['Agnes Water and 1770',-24.2125,151.9033,43,'#agnes-water']
 ];
 
 // Add markers to the map.
